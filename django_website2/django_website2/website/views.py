7009f3eda339b97618f83da802c8c93aa08a996a
T.NAKAMURA <t.nakamura@TNAKAMURAnoMacBook-puro.local>
2021-11-03 22:29:19 +0900

    first

---
+++ django_website2/django_website2/website/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"]="taichi"
+        return ctxt
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_services"]=12345678
+        ctxt["skills"]=[
+            "python",
+            "C++",
+            "Javascript",
+
+        ]
+        return ctxt
